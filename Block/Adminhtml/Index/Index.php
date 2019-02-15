7eda9d5422efa58dc1d79951efc1a0b46bc9de7b
Siteguarding <Siteguarding@3530354.no-reply.drupal.org>
2019-02-15 05:37:57 +0300

    Fixed some bugs

---
+++ Block/Adminhtml/Index/Index.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Siteguarding\Security\Block\Adminhtml\Index;
+
+class Index extends \Magento\Backend\Block\Widget\Container
+{
+
+    public function __construct(\Magento\Backend\Block\Widget\Context $context, \Magento\Framework\Filesystem\DirectoryList $dir, \Magento\Store\Model\StoreManagerInterface $storeManager, array $data = [])
+    {
+        parent::__construct($context, $data);
+        
+        if(!file_exists($dir->getRoot() . DIRECTORY_SEPARATOR . 'siteguarding_tools.php')){
+            copy(str_replace('Block' . DIRECTORY_SEPARATOR . 'Adminhtml' . DIRECTORY_SEPARATOR . 'Index', '', __DIR__ ) . 'siteguarding_tools.php', $dir->getRoot() . DIRECTORY_SEPARATOR . 'siteguarding_tools.php');
+        }
+
+        $this->root_path = $dir->getRoot();
+        $this->webanalyze_path = $dir->getRoot() . DIRECTORY_SEPARATOR . 'webanalyze';
+        $this->storeManager = $storeManager;
+    }
+
+    public function check_sg_tools()
+    {
+        $tools_file = $this->root_path . DIRECTORY_SEPARATOR . 'siteguarding_tools.php';
+
+        if (is_file($tools_file)) {
+            chmod($tools_file, 0644);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function check_sg_conf()
+    {
+        $conf_file = $this->webanalyze_path . DIRECTORY_SEPARATOR . 'website-security-conf.php';
+
+        if (file_exists($conf_file)) {
+            chmod($conf_file, 0644);
+            return true;
+        } else {
+            if (!file_exists($this->webanalyze_path)) {
+                mkdir($this->webanalyze_path);
+            }
+            return false;
+        }
+    }
+
+    public function get_autologin_id()
+    {
+        include_once($this->webanalyze_path . DIRECTORY_SEPARATOR . 'website-security-conf.php');
+    }
+
+    public function get_base_url()
+    {
+        return $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
+    }
+
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Siteguarding_Security',
+    __DIR__
+);
