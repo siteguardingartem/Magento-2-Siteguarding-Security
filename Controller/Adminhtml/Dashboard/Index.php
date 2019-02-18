b3dbbef74bd432cb12880e0e8c8ab8442ad3f2a3
Siteguarding <Siteguarding@3530354.no-reply.drupal.org>
2019-02-18 05:47:00 +0300

    new files

---
+++ Controller/Adminhtml/Dashboard/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Siteguarding\Security\Controller\Adminhtml\Dashboard;
+
+use Magento\Backend\App\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        die("Controller backend");
+    }
+}
